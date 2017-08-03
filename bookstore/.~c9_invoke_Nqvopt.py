559fb0b077a0fb1b39571b8f6e761e8cc5f06f59
Juan Chaves <chavesjuanm@gmail.com>
2017-08-03 02:37:16 +0000

    finished project

--- bookstore/.~c9_invoke_Nqvopt.py
+++ bookstore/.~c9_invoke_Nqvopt.py
@@ -31,44 +31,30 @@ def get_author_by_name(bookstore, name):
 
 
 def get_author_by_id(bookstore, author_id):
-    authors = bookstore['authors']
+
     for author in authors:
+        i_d = author['id']
         if author_id == author['id']:
-            break
-    return author
+            return author
 
 
 def add_book(bookstore, title, isbn, author_id):
-    _id = id(isbn)
     book = {'title' : title, 
             'isbn' : isbn, 
-            'author_id' : author_id,
-            'id': _id
-            }
+            'id' : author_id}
     bookstore['books'].append(book)
     return book
 
 def get_book_by_title(bookstore, title):
     books = bookstore['books']
     for book in books:
-        if book['title'] == title:
-            break
-    return book
+        if book == title:
+            return book
 
 
 def get_book_by_id(bookstore, book_id):
-    books = bookstore['books']
-    for book in books:
-        if book_id == book['id']:
-            break
-    return book
+    pass
 
 
 def get_books_by_author(bookstore, author_id):
-    books = bookstore['books']
-    books_by_author = []
-    for book in books:
-        if book['author_id'] == author_id:
-            books_by_author.append(book)
-    return books_by_author
-    
+    pass
